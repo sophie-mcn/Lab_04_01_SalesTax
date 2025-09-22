e4ed86e32a5b3f4df5e9c2fefe4fc6315dd1a155
sophie-mcn <mcnamask@mail.uc.edu>
2025-09-22 08:24:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        double itemPrice = 17.82;
+        double TAX_PERCENT = 0.05;
+        double salesTax = itemPrice * TAX_PERCENT;
+        double totalCost = itemPrice + salesTax;
+
+        System.out.println("The sales tax on " + itemPrice + " is " + salesTax);
+        System.out.println("The total cost is " + totalCost);
+
+
+    }
+}
